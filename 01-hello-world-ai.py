8f8782d0f87d55df0aaa0fc29c6b3260508d5f11
blakc-ai <dejiakande8@gmail.com>
2025-04-22 17:03:34 -0500

    Update 01-hello-world-ai.py

--- 01-hello-world-ai.py
+++ 01-hello-world-ai.py
@@ -1,4 +1,6 @@
-
+# Project: Hello World AI Script
+# Author: Deejay Blakc
+# Description: My first AI project using Python. This script simply prints a welcome message to mark the start of my journey in AI Engineering.
 # Hello World AI Script
 print("Welcome to Deejay Blakc's AI journey!")
 print("This is my first Python project on GitHub.")
